b4402e392e7327a19503586061d86840897471aa
aag-github <aag_uk@yahoo.com>
2020-04-26 11:48:24 +0200

    branchout.sh version.txt

---
+++ branchout.sh
@@ -0,0 +1,11 @@
+git checkout $1 version.txt
+V=`cut -f 1 -d "." version.txt`
+git reset HEAD version.txt
+git checkout version.txt
+let NV=V+1
+FULL_VERSION="$NV.0"
+echo $FULL_VERSION
+echo $FULL_VERSION > version.txt
+git tag $FULL_VERSION $1
+git branch "v/$FULL_VERSION" $1
+git push origin "v/$FULL_VERSION" --tags
