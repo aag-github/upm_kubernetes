990b6a580ba9fed6dac1002c72577ab8f09d7775
aag-github <aag_uk@yahoo.com>
2020-04-26 16:25:40 +0200

    fix branchout

--- branchout.sh
+++ branchout.sh
@@ -6,6 +6,8 @@ let NV=V+1
 FULL_VERSION="$NV.0"
 echo $FULL_VERSION
 echo $FULL_VERSION > version.txt
+git add version.txt
+git commit -m "New version: $FULL_VERSION"
 git tag $FULL_VERSION $1
 git branch "v/$FULL_VERSION" $1
 git push origin "v/$FULL_VERSION" --tags
