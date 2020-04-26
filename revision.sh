13028a1beb185fb13bb3f56f684924af0aee837c
aag-github <aag_uk@yahoo.com>
2020-04-26 16:33:16 +0200

    revision.sh

---
+++ revision.sh
@@ -0,0 +1,13 @@
+git checkout $1 version.txt
+V=`cut -f 1 -d "." version.txt`
+REV=`cut -f 2 -d "." version.txt`
+git reset HEAD version.txt
+git checkout version.txt
+let NEW_REV=REV+1
+FULL_VERSION="$V.$NEW_REV"
+echo $FULL_VERSION
+echo $FULL_VERSION > version.txt
+git add version.txt
+git commit -m "New sub-version: $FULL_VERSION"
+git tag $FULL_VERSION $1
+git push --tags
